91aafd079102efd0093894d59aeda8789915fb0d
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2021-01-06 13:45:34 -0800

    create elementid variables

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+const result = document.getElementById('result')
+const more = document.getElementById('more')
+
+const apiURL = 'https://api.lyrics.ovh';
+
+
+// Event Listener
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    
+})
