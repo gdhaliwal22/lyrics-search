80f0a0ca45da0382354b06701dc9e39a70c86c51
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2021-01-11 10:27:46 -0800

    created next previous buttons for more songs

--- script.js
+++ script.js
@@ -15,22 +15,36 @@ async function searchSongs(term) {
 
 // Show song and artist in DOM
 function showData(data) {
-    let output = '';
-    data.data.forEach(song => {
-        output += `
-         <li>
-         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-         <button class="btn" data-artist="${song.artist.name}" 
-         data-songtitle="${song.title}">Get Lyrics</button>
-         <li>
-         `;
-    });
+    console.log(data)
     result.innerHTML = `
-        <ul class="songs">
-        ${output}
-        </ul>
-        `
+    <ul class="songs">
+      ${data.data
+            .map(
+                song => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+            )
+            .join('')}
+    </ul>
+  `;
+
+    if (data.prev || data.next) {
+        more.innerHTML = `
+      ${data.prev
+                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+                : ''
+            }
+      ${data.next
+                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+                : ''
+            }
+    `;
+    } else {
+        more.innerHTML = '';
+    }
 }
+
 // Event Listener
 form.addEventListener('submit', e => {
     e.preventDefault();
