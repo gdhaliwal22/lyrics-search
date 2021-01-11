3b68aebf19f361d2a1e74c750e314237f9376914
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2021-01-11 10:50:49 -0800

    created line breaks for lyrics, lyrics now displaying in DOM

--- script.js
+++ script.js
@@ -52,11 +52,19 @@ async function getMoreSongs(url) {
     showData(data);
 }
 
+// Get lyrics for song
 async function getLyrics(artist, songTitle) {
     const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
     const data = await res.json();
 
-    console.log(data)
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `
+            <h2><strong>${artist}</strong> - ${songTitle}</h2>
+            <span>${lyrics}</span>
+        `;
+
+    more.innerHTML = ''
 }
 
 // Event Listener
